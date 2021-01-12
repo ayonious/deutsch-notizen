38b361b07e4f14c948a629fdfd1d6f7b79e5d297
Nahiyan Kamal <noyanayihan@gmail.com>
2021-01-12 12:38:47 +0100

    fix: update readme

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   docs: {
     Basics: ["JenigeJener", "Konjunktiv12"],
     Fortgeschrittene: ["Redevendung", "SprechenStyling", "Abkürzungen"],
-    B2: [ "Brief", "Satzbau", "B2Wortschatz" ]
+    B2: [ "Brief", "B2Wortschatz" ]
   }
 }
 
