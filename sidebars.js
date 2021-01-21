f2c93427be7b79a10f23d76a1484d58ff229571f
Nahiyan Kamal <noyanayihan@gmail.com>
2021-01-21 23:57:58 +0100

    feat: update

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   docs: {
     Basics: ["JenigeJener", "Konjunktiv12"],
     MittelSchritte: ["PlusquimPerfekt", "Machen"],
-    Fortgeschrittene: ["Redevendung", "SprechenStyling", "Abkürzungen", "DerenDessen"],
+    Fortgeschrittene: ["Redevendung", "SprechenStyling", "Abkürzungen", "DerenDessen", "Passiv"],
     B2: [ "B2MundlichPrufung", "B2Brief", "B2Wortschatz" ]
   }
 }
